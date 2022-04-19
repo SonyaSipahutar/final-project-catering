14aa42472e53044b5471ed468b892b7032a33ddb
SonyaSipahutar <sonyasipahutar8@gmail.com>
2022-04-19 22:56:11 +0700

    Add menuitem model validation, valid with the atribute

--- spec/models/menu_item_spec.rb
+++ spec/models/menu_item_spec.rb
@@ -4,4 +4,9 @@ RSpec.describe MenuItem, type: :model do
   it "has a valid factory" do
     expect(FactoryBot.build(:menu_item)).to be_valid
   end
+
+  it "valid with a name, price and description" do
+    expect(FactoryBot.build(:menu_item)).to be_valid
+  end
+
 end
