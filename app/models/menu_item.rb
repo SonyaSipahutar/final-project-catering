b5580ca51f3979d494afcae1cd35d470b7247a29
SonyaSipahutar <sonyasipahutar8@gmail.com>
2022-04-19 23:28:51 +0700

    Add validation for MenuItem, invalid without name or price

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,2 +1,4 @@
 class MenuItem < ApplicationRecord
+    validates :name, presence:true
+    validates :price, presence:true
 end
--- spec/models/menu_item_spec.rb
+++ spec/models/menu_item_spec.rb
@@ -5,8 +5,20 @@ RSpec.describe MenuItem, type: :model do
     expect(FactoryBot.build(:menu_item)).to be_valid
   end
 
-  it "valid with a name, price and description" do
+  it "is valid with a name, price and description" do
     expect(FactoryBot.build(:menu_item)).to be_valid
+  end
+
+  it "is invalid without name" do
+    item = FactoryBot.build(:menu_item, name:nil)
+    item.valid?
+    expect(item.errors[:name]).to include("can't be blank")
+  end
+
+  it "is invalid without price" do
+    item = FactoryBot.build(:menu_item, price:nil)
+    item.valid?
+    expect(item.errors[:price]).to include("can't be blank")
   end
 
 end
